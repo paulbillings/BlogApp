27d2e1d59e40948a2feb0d7bd2e94aca3ec51cc4
Paul Billings <paulbillings101@yahoo.co.uk>
2018-02-27 11:52:18 +0000

    Added delete route

--- app.js
+++ app.js
@@ -104,6 +104,17 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+// delete route
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    }) 
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Blog App server has started!!");
 });
